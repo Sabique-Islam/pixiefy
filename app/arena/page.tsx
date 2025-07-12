6dbe86dd33a79c3ce7d4c1efe67b8e08aab430bf
sabique <sabiqueislam7@gmail.com>
2025-07-12 21:13:37 +0530

    chore: add page

---
+++ app/arena/page.tsx
@@ -0,0 +1,10 @@
+import ProfileForm from "@/components/ProfileForm";
+
+export default function Arena() {
+  return (
+    <div className="text-white p-10">
+      <ProfileForm />
+      
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-import ProfileForm from '@/components/ProfileForm'
 import Navbar from '@/components/Navbar'
 import { Hero } from '@/components/Hero'
+import Footer from '@/components/Footer'
+import HowToUse from '@/components/HowToUse'
 
 export default function Home() {
   return (
       <div>
         <Navbar />
         <Hero/>
-        <ProfileForm />
+        <HowToUse/>
+        <Footer/>
       </div>
   )
 }
